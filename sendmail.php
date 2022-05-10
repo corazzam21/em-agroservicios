28ac28f1c2a74a917228f2f0e177913e9defd580
Corazza Mariano <marianojesuscorazza@gmail.com>
2022-05-10 20:47:03 -0300

    solucionado contacto

--- sendmail.php
+++ sendmail.php
@@ -1,6 +1,10 @@
 <?php
-require 'PHPMailer/PHPMailerAutoload.php';
-
+ use PHPMailer\PHPMailer\PHPMailer;
+ use PHPMailer\PHPMailer\Exception;
+ use PHPMailer\PHPMailer\SMTP;
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
 //Create a new PHPMailer instance
 $mail = new PHPMailer();
 $mail->IsSMTP();
